90c9a4d92fdf8c3bedb2384411f7b8cd21c735c9
vien pham <vienpham2019@gmail.com>
2019-12-08 02:44:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  last_two_element = array.pop(2)
+  return last_two_element
+end
+
+def using_unshift(array)
+  array.unshift()
+end
+
+def shift_with_args(array)
+  first_two_items = array.unshift(2)
+  return first_two_items
+end
+
